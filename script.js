a720b04a27e2da9d070a3415bee1ce35d0d76aed
Jair Hernández <jair_097@hotmail.com>
2022-02-15 19:27:50 -0800

    Segundo cambio

--- script.js
+++ script.js
@@ -1,180 +1,104 @@
-// class Node
-// {
-//     constructor(data)
-//     {
-//         this._data = data;
-//         this._next = null;
-//     }
-// }
-// class LinkedList
-// {
-//     constructor(head = null)
-//     {
-//         this._head = head;
-//         this._size = 0;
-//     }
-
-//     insert(data)
-//     {
-//         let node = new Node(data);
-//         if(this._head == null)
-//         {
-//             this._head = node;
-//         }
-//         else
-//         {
-//             let current = this._head;
-//             for(let i = 0; i < this._size; i++)
-//             {
-//                 if(current._next == null)
-//                 {
-//                     current._next = node; //explicación de current desde el minuto 44 aprox
-//                 }
-//                 else
-//                 {
-//                     current = current._next;
-//                 }
-//             }
-//         }
-//         this._size++;
-//     }
-
-//     delete(data)
-//     {
-//         let current = this._head;
-//         let anterior = current;
-//         for(let i = 0; i < this._size; i++)
-//         {
-//             if(current._data == data)
-//             {
-//                 anterior._next = current._next;
-//             }
-//             else
-//             {
-//                 anterior = current;
-//                 current = current._next;
-//             }
-//         }
-//         this._size--; 
-//     }
-
-//     search(data)
-//     {
-//         let current = this._head;
-//         for(let i = 0; i < this._size; i++)
-//         {
-//             if(current._data == data)
-//             {
-//                 console.log(current);
-//                 return current;
-//             }
-//             else
-//             {
-//                 current = current._next;
-//             }
-//         }
-//     }
-
-//     print()
-//     {
-//         let current = this._head;
-//         let res = "";
-//         while(current._next != null)
-//         {
-//             res += `${current._data} -> `
-//             current = current._next;
-//         }
-//         console.log(res);
-//     }
-
-//     insertTail(data)
-//     {
-//         let current = this._head;
-//         for(let i = 0; i < this._size; i++)
-//         {
-//             if(i == 0)
-//             {
-//                 if(current._data == data)
-//                 {
-//                     console.log("Este dato ya se encuentra dentro de la lista enlazada y es el primer nodo");
-//                     break;
-//                 }
-//             } 
-//             else if(i > 0)
-//             {
-//                 current = current._next;
-//                 if(current._data == data)
-//                 {
-//                     console.log("Este dato ya se encuentra dentro de la lista enlazada");
-//                     break;
-//                 }
-//                 else if(current._data != data && current._next == null)
-//                 {
-//                     console.log("Puedes ingresar este dato");
-//                     this.insert(data);
-//                 }
-//             }
-//         }
-//     }
-
-//     printMoreThan(number)
-//     {
-//         let current = this._head;
-//         let res = "";
-//         while(current._next != null)
-//         {
-//             if(current._data > number)
-//             {
-//                 res += `${current} -> `;
-//             }
-//             current = current._next;
-//         }
-//         console.log(res);
-//     }
-// }
-
+class Node {
+    constructor(data)
+    {
+        this._data = data
+        this._left = null
+        this._right = null
+    }
+}
+class BinaryTree {
+    constructor(root = null)
+    {
+        this._root = root
+    }
+    insert(data)
+    {
+        let node = new Node(data)
+        if(this._root == null)
+        {
+            this._root = node
+        } else
+        {
+            let current = this._root
+            while(true) 
+            {
+                if(current._right != null && current._data.name < node._data.name)
+                {
+                    current = current._right
+                }
+                else if(current._left != null && current._data.name > node._data.name)
+                {
+                    current = current._left
+                }
+                else
+                {
+                    break
+                }
+            }
+            if(current._right == null && current._data.name < node._data.name)
+            {
+                current._right = node
+            }
+            else if(current._left == null && current._data.name > node._data.name)
+            {
+                current._left = node
+            }
+        }
+    } 
+    
+    search(data) 
+    {
+        if(this._root._data.name == data)
+        {
+            return this._root
+        }
+        else
+        {
+            let current = this._root
+            while(true)
+            { 
+                if(current._right != null && current._data.name < data)
+                {
+                    current = current._right
+                }
+                else if(current._left != null && current._data.name > data)
+                {
+                    current = current._left
+                }
+                else
+                {
+                    break
+                }
+            }
+            console.log(current)
+        }
+    }
+}
 async function getData()
 {
     let base = 'https://swapi.dev/api/';
     let people = 'people/';
-    // let planets = 'planets/';
-    // let films = 'films/';
-    // let species = 'species/';
-    // let vehicles = 'vehicles/';
-    // let starships = 'starships/';
     console.log("Cargando datos ⌛️⏳⌛️");
-    for(let i = 1; i <84;i++)
+    for(let i = 1; i < 84;i++)
     {
-        if(i != 17)
+        if(i == 17)
+        {
+            continue;
+        }
+        else
         {
             const response = await fetch(`${base}${people}${i}`);
             const json = await response.json();
             data.push(json);
+            tree.insert(data[data.length-1]);
         }
     }
     console.log("Datos cargados ✅");
-    // list.prin"t();
-    // let file = "data.json";
-    // let myRequest = new Request(file);
-    // fetch(myRequest)
-    //     .then(function(response){return response.json();})
-    //     .then(function(information)
-    //     {
-    //         console.log(information);
-    //         let list = new LinkedList();
-    //         let element = new Node(information);
-    //         list.insert(element);
-    //         // for(let i = 0; i < information.length; i++)
-    //         // {
-    //         //     let element = new Node (information[0][i]);
-    //         //     list.insert(element);
-    //         //     data.push(element);
-    //         // }
-    //         list.print();
-    //     })
+    showAllData(data);
+    // console.log(tree);
+    // console.log(data);
 }
-
-function searchData(array)
+function searchDataArray(array)
 {
     let value = document.getElementById("searchBox").value;
     if(value == "")
@@ -187,19 +111,36 @@ function searchData(array)
         {
             if(array[j].name.includes(value))
             {
-                let defaultText = array[j].name;
-                console.log(`${value} y ${defaultText}  son iguales`)
+                console.log(`${value} SI se encuentra en la api y su posición es ${j}`);
+                position = j;
                 console.log(array[j]);
                 break;
-            }
-            else
-            {
-                let defaultText = array[j].name;
-                console.log(`${value} y ${defaultText} no son iguales`)
             }
         }
         document.getElementById("searchBox").value =  "";
     }
 }
+function searchDataBinaryTree()
+{
+    let value = document.getElementById("searchBox").value;
+    tree.search(value);
+}
+function showAllData(array)
+{
+    console.log(array);
+    let charactersDataDiv = document.getElementById("charactersDataDiv");
+    for(let k = array.length; k = 0; k--)
+    {
+        let name = `<h4>${array[k].name}</h4>`;
+        let gender = `<h4>${array[k].gender}</h4>`;
+        let height = `<h4>${array[k].height}</h4>`;
+        charactersDataDiv.insertAdjacentHTML("afterend",height);
+        charactersDataDiv.insertAdjacentHTML("afterend",gender);
+        charactersDataDiv.insertAdjacentHTML("afterend",name);
+    }
+}
+let position = null;
 let data = [];
+let tree = new BinaryTree();
 getData();
+
